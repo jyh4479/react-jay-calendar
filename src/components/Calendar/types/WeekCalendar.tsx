5606b92c14fd06ee04bbf7bac093ecd58c4b28cb
jyh4479 <jyh.dev@gmail.com>
2022-09-12 16:51:56 +0900

    [FEAT]: Test Week Calendar
    - Logic refactoring 추가 예정

--- src/components/Calendar/types/WeekCalendar.tsx
+++ src/components/Calendar/types/WeekCalendar.tsx
@@ -1,6 +1,7 @@
 import React, {useEffect, useState} from 'react';
 import {CalendarProps} from "../Calendar";
 import {getWeekOfDate} from "../utils/Time";
+import {getDay} from "../utils/Format";
 
 const WeekCalendar = (props: CalendarProps) => {
 
@@ -13,7 +14,25 @@ const WeekCalendar = (props: CalendarProps) => {
     }, [curDate])
 
     return (
-        <div>{curWeek.map(test => <div>{new Date(test).toString()}</div>)}</div>
+        <div>
+            <div>
+                <button
+                    onClick={() => setCurDate(prev => new Date(new Date(prev).setDate(new Date(prev).getDate() - 7)).getTime())}>이전주
+                </button>
+                <button
+                    onClick={() => setCurDate(prev => new Date(new Date(prev).setDate(new Date(prev).getDate() + 7)).getTime())}>다음주
+                </button>
+                <div style={{display: "flex"}}>
+                    {curWeek.map(test => <div style={{width: "50px"}}>{getDay(test)}</div>)}
+                </div>
+                <div style={{display: "flex"}}>
+                    {curWeek.map(test => <div style={{width: "50px"}}>{new Date(test).getDate()}</div>)}
+                </div>
+            </div>
+            <div>
+                Body (하루 Time 들어갈 공간)
+            </div>
+        </div>
     )
 };
 
---
+++ src/components/Calendar/utils/Format.ts
@@ -0,0 +1,21 @@
+export const getDay = (date: string | number | Date) => {
+
+    const day = new Date(date).getDay();
+
+    switch (day) {
+        case 0:
+            return "Sun";
+        case 1:
+            return "Mon";
+        case 2:
+            return "Tue";
+        case 3:
+            return "Wed";
+        case 4:
+            return "Thu";
+        case 5:
+            return "Fri";
+        case 6:
+            return "Sat";
+    }
+}
